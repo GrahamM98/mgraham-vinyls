6561a0f24c86ab386bc6b97f42bfa0de02cb5d93
Michael Graham <michael@thegrahamfam.com>
2021-09-25 12:03:29 -0500

    remove-icon
    removes the react icon from the home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,6 @@ function App() {
     return (
         <div className="App">
          <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
            Testing connection to API...
           </p>
